1d5b44b481788f4f97670230c5d00921570109f6
lbwhs123 <lylabaca2028@gmail.com>
2024-10-28 11:41:24 -0700

    loops
    making loops scripting the loops

---
+++ ScriptingProjectP4Baca/Assets/scripts/ForLoops.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoops : MonoBehaviour
+{
+    int numEnemies = 3;
+
+
+
+    void Start()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+    }
+}
